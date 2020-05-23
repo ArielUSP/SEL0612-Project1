e54693e388e2431f45db793b2b3fa1a1aca52cdf
Samuel Libardi Godoy <samlibardi@outlook.com>
2020-05-23 20:23:22 -0300

    modelo do tline.pdf

--- ftdt.py
+++ ftdt.py
@@ -1,34 +1,45 @@
 
 
-def ftdt(R, G, L, C, RL, v0, i0, dz, dt, nmax):
+def ftdt(L, C, RL, i0, v0, dz, dt, vph, R=0, G=0):
 	v = v0
 	i = i0
 
 	kmax = len(i)
 
-	c1 = 2 * dt / (dt * dz * R + 2 * dz * L)
-	c2 = (2 * L - dt * R) / (2 * L + dt * R)
-	c3 = -(2 * dt) / (dt * dz * G + 2 * dz * C)
-	c4 = (2 * C - dt * G) / (2 * C + dt * G)
-	
-	n = 0
+	d = dt / dz
+	re = d / L
+	rh = d / C
 
-	while n < nmax:
-		n = n+1
-		iprox = [c1 * (v[k + 1] - v[k - 1]) + c2 * i[k] for k in range(1, kmax - 1)]
+	while True:
+		iprox = [0] + [i[k] - re*(v[k]-v[k-1]) for k in range(1,kmax)]
 
-		if RL == float("inf"):
-			iprox = [0] + iprox
-		elif RL == 0:
-			iprox = [iprox[0]] + iprox
+		# if RL == float("inf"):
+		# 	iprox[0] = 0
+		# elif RL == 0:
+		# 	iprox[0] = iprox[1]
 
-		vprox = [c3 * (iprox[k + 1] - iprox[k]) + c4 * v[k + 1] for k in range(0, kmax)]
+		vprox = [0] + [v[k] - rh * (iprox[k + 1] - iprox[k]) for k in range(1, kmax - 1)] + [0]
+		
+		#condições de contorno para V
+		vprox[0] = v[1] + ((vph*dt - dz)/(vph*dt + dz))*(vprox[1] - v[0])
+		vprox[-1] = v[-2] + ((vph*dt - dz)/(vph*dt + dz))*(vprox[-2] - v[0])
 
 		#TODO: resolver para carga RL resistiva
 		
-		yield i, v
+		yield iprox, vprox
 
-gen = (ftdt(0,1,1,0,0,[0,0,0,0,12],[0,0,0,0,0],0.1,0.1,10))
+		v = vprox
+		i = iprox
+
+gen = (ftdt(1,1,0,[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,6],0.1,0.1,10))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
 print(next(gen))
 print(next(gen))
 print(next(gen))
